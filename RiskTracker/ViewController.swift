84c7d41dc86b85f0bbbf838e92a3d0128ed35040
ChenYingYu <alan831007@gmail.com>
2018-07-28 16:50:22 +0800

    Create detail labels and use pickerView as inputView of natureTextField.

--- RiskTracker/ViewController.swift
+++ RiskTracker/ViewController.swift
@@ -11,10 +11,12 @@ import GoogleMaps
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var natureLabel: UILabel!
+    @IBOutlet weak var seriousnessLabel: UILabel!
+    @IBOutlet weak var shortDescription: UILabel!
+    @IBOutlet weak var natureTextField: UITextField!
     @IBOutlet weak var mapView: GMSMapView!
     @IBOutlet weak var imageView: UIImageView!
-    
-    var imagePicker: UIImagePickerController!
     
     @IBAction func takePhotos(_ sender: UIButton) {
         imagePicker =  UIImagePickerController()
@@ -24,10 +26,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(imagePicker, animated: true, completion: nil)
     }
     
+    var imagePicker: UIImagePickerController!
+    let nature = ["Cars","Roads","People"]
+    let naturePickerView = UIPickerView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         setUpMapView()
+        setUpPickerView()
+        setUpTextField()
     }
     
     func setUpMapView() {
@@ -35,6 +43,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         mapView.camera = GMSCameraPosition.camera(withLatitude: locationManager.getUserLatitude(), longitude: locationManager.getUserLongitude(), zoom: 15.0)
         mapView.isMyLocationEnabled = true
         mapView.delegate = self
+    }
+    
+    func setUpPickerView() {
+        naturePickerView.delegate = self
+        naturePickerView.dataSource = self
+    }
+    
+    func setUpTextField() {
+        natureTextField.inputView = naturePickerView
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
@@ -68,6 +85,22 @@ extension CLLocationManager {
     }
 }
 
-extension ViewController: GMSMapViewDelegate {
-
+extension ViewController: GMSMapViewDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return nature.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return nature[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        natureTextField.text = nature[row]
+        natureTextField.resignFirstResponder()
+    }
 }
