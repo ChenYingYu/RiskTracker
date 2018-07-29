5b78cf582e625fe7f78e35e7227b4e545c3ed985
ChenYingYu <alan831007@gmail.com>
2018-07-29 12:34:54 +0800

    Implement userChatLabel and secondChatbotLabel.

--- RiskTracker/RTChatbotViewController.swift
+++ RiskTracker/RTChatbotViewController.swift
@@ -11,11 +11,15 @@ import AssistantV1
 
 class RTChatbotViewController: UIViewController {
     @IBOutlet weak var firstChatbotLabel: UILabel!
+    @IBOutlet weak var userChatLabel: UILabel!
+    @IBOutlet weak var secondChatbotLabel: UILabel!
+    @IBOutlet weak var chatTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        chatTextField.delegate = self
         activateChatbot()
     }
 
@@ -37,12 +41,37 @@ class RTChatbotViewController: UIViewController {
             }
             context = response.context
         }
-        let input = InputData(text: "2")
-        let request = MessageRequest(input: input, context: context)
-        assistant.message(workspaceID: workspaceID, request: request, failure: failure) {
-            response in
-            print(response.output.text)
-            context = response.context
+    }
+}
+
+extension RTChatbotViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == chatTextField {
+            textField.resignFirstResponder()
+            userChatLabel.text = textField.text
+            userChatLabel.isHidden = false
+            let username = "681ad1db-ad92-4885-8833-63190dec864e"
+            let password = "WvREbOy1Exb4"
+            let version = "2018-07-29" // use today's date for the most recent version
+            let workspaceID = "14c5902c-f1de-4c75-b8ee-537b6ed2bd99"
+            let assistant = Assistant(username: username, password: password, version: version)
+            let failure = { (error: Error) in print(error) }
+            var context: Context? // save context to continue conversation
+            if let text = textField.text {
+                let input = InputData(text: text)
+                let request = MessageRequest(input: input, context: context)
+                assistant.message(workspaceID: workspaceID, request: request, failure: failure) {
+                    [weak self] response in
+                    print(response.output.text)
+                    context = response.context
+                    DispatchQueue.main.async {
+                        self?.secondChatbotLabel.text = response.output.text[0]
+                        self?.secondChatbotLabel.isHidden = false
+                    }
+                }
+            }
+            return false
         }
+        return true
     }
 }
